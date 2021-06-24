3527284e550428871633ae5e6f71ecf0f54afee9
EmilbekDzhumakeev <emilbek.dzhumakeev17@gmail.com>
2021-06-23 20:11:53 -0400

    Modified MusicLibraryStarterCode changed /api/products to /api/music, tested server in Postman able to: GET all songs in datastorage, GET specific song based off it's 'id', POST add new data to datastorage, PUT to update data, DELETE CRUD functionality, and middleware works as expected for POST/PUT

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express'); 
 const repoContext = require('./repository/repository-wrapper');
 const cors = require('cors');
-const { validateProduct } = require('./middleware/products-validation');
+const { validateSong } = require('./middleware/songs-validation');
 
 const app = express();
  
@@ -13,31 +13,31 @@ app.listen(3000, function () {
     console.log("Server started. Listening on port 3000. ");
     });
  
-app.get('/api/products', (req, res) => {
-    const products = repoContext.products.findAllProducts(); 
-    return res.send(products);
+app.get('/api/songs', (req, res) => {
+    const songs = repoContext.songs.findAllSongs(); 
+    return res.send(songs);
     }); 
      
-app.get('/api/products/:id', (req, res) => {
+app.get('/api/songs/:id', (req, res) => {
     const id = req.params.id; 
-    const product = repoContext.products.findProductById(id);
-    return res.send(product);
+    const songs = repoContext.songs.findSongById(id);
+    return res.send(songs);
     }); 
 
-app.post('/api/products',  (req, res) => {
-    const newProduct = req.body;
-    const addedProduct = repoContext.products.createProduct(newProduct);
-    return res.send(addedProduct);
+app.post('/api/songs', [validateSong], (req, res) => {
+    const newSong = req.body;
+    const addedSong = repoContext.songs.createSong(newSong);
+    return res.send(addedSong);
     }); 
  
-app.put('/api/products/:id', [validateProduct],(req, res) => { 
+app.put('/api/songs/:id',[validateSong], (req, res) => { 
     const id = req.params.id;
-    const productPropertiesToUpdate = req.body;
-    const updatedProduct = repoContext.products.updateProduct(id, productPropertiesToUpdate);
-        return res.send(updatedProduct)
+    const songPropertiesToUpdate = req.body;
+    const updatedSong = repoContext.songs.updateSong(id, songPropertiesToUpdate);
+        return res.send(updatedSong)
     }); 
 
-app.delete('/api/products/:id', (req, res) => {
+app.delete('/api/songs/:id', (req, res) => {
     const id = req.params.id;
-    const updatedDataSet = repoContext.products.deleteProduct(id); return res.send(updatedDataSet);
+    const updatedDataSet = repoContext.songs.deleteSong(id); return res.send(updatedDataSet);
     });
